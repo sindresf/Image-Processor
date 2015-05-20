748721b711c1f59e36e5e2b80c1455e2c75eb338
mr.Fjermestad <sindresfjermestad@gmail.com>
2015-05-20 17:54:38 +1000

    removed annoying sysos and small cleaners like so.

--- src/mosaic/ImagesToMosaic.java
+++ src/mosaic/ImagesToMosaic.java
@@ -33,10 +33,6 @@ public class ImagesToMosaic {
 		// functions (duh)
 	}
 
-	// TODO decide how lenient to be with input. Rows and squaresPrRow should
-	// match up with the number of images for a nice picture. So: make the best
-	// of it, or just abort and tell user?
-	// maybe have a checkinput inbetween step?
 	public static void makeCompressedSquareMosaic(
 			ArrayList<BufferedImage> images, int mosaicWidth, int mosaicHeight,
 			int rows, int squaresPrRow) {
@@ -80,7 +76,6 @@ public class ImagesToMosaic {
 
 		// setup
 		int imageCount = componentImages.size();
-		System.out.println("imageCount = " + imageCount);
 		int mosaicWidth = 0;
 		int avrgHeight = 0;
 		for (BufferedImage image : componentImages) {
@@ -88,15 +83,14 @@ public class ImagesToMosaic {
 			avrgHeight += image.getHeight();
 		}
 		int mosaicHeight = (int) (avrgHeight / (imageCount + 0.0));
-		System.out.println("mosaic width and height: " + mosaicWidth + ", "
-				+ mosaicHeight);
 		BufferedImage mosaic = new BufferedImage(mosaicWidth, mosaicHeight,
 				BufferedImage.TYPE_INT_ARGB);
 		Graphics g = mosaic.getGraphics();
 
 		// for draw scaling
 		int pieceWidth = (int) (mosaicWidth / ((components * imageCount) + 0.0));
-		System.out.println("draw pieceWidth = " + pieceWidth);
+		int leftOver = mosaicWidth - (pieceWidth * components * imageCount);
+		System.out.println("leftover pixels: " + leftOver);
 
 		int imageIndex = 0;
 		// going through the images
@@ -113,7 +107,6 @@ public class ImagesToMosaic {
 				int width = imagePieceWidth;
 				int height = componentImg.getHeight();
 
-				System.out.println("piece sub X: " + xSub);
 				// THIS IS WHOLLY RELIANT ON THE IMAGE'S DOMENTIONS!
 				BufferedImage subImg = componentImg.getSubimage(xSub, 0, width,
 						height);
@@ -123,7 +116,6 @@ public class ImagesToMosaic {
 				int xDraw = (piece * pieceWidth * imageCount) + offset;
 
 				// THIS IS IRRESPECTIVE OF IMAGE DIMENTIONS"
-				System.out.println("piece draw X: " + xDraw);
 				g.drawImage(subImg, xDraw, 0, pieceWidth, mosaicHeight, null);
 			}
 			imageIndex++;
@@ -144,7 +136,6 @@ public class ImagesToMosaic {
 			ArrayList<BufferedImage> componentImages, int components) {
 		// setup
 		int imageCount = componentImages.size();
-		System.out.println("imageCount = " + imageCount);
 		int avrgWidth = 0;
 		int avrgHeight = 0;
 		for (BufferedImage image : componentImages) {
@@ -153,8 +144,6 @@ public class ImagesToMosaic {
 		}
 		int mosaicWidth = (int) (avrgWidth / (imageCount + 0.0));
 		int mosaicHeight = (int) (avrgHeight / (imageCount + 0.0));
-		System.out.println("mosaic width and height: " + mosaicWidth + ", "
-				+ mosaicHeight);
 		BufferedImage mosaic = new BufferedImage(mosaicWidth, mosaicHeight,
 				BufferedImage.TYPE_INT_ARGB);
 		Graphics g = mosaic.getGraphics();
@@ -162,12 +151,10 @@ public class ImagesToMosaic {
 		// for draw scaling
 		int pieceWidth = (int) (mosaicWidth / ((components * imageCount) + 0.0));
 		int leftOver = mosaicWidth - (pieceWidth * components * imageCount);
-		System.out.println("draw pieceWidth = " + pieceWidth);
 		System.out.println("leftover pixels: " + leftOver);
 
 		int imageIndex = 0;
 		// drawing/combining
-		System.out.println("imageCount = " + imageCount);
 		for (BufferedImage componentImg : componentImages) {
 			int imagepieceWidth = (int) (componentImg.getWidth() / ((components * imageCount) + 0.0));
 			System.out.println();
@@ -181,14 +168,12 @@ public class ImagesToMosaic {
 						* imagepieceWidth;
 				int width = imagepieceWidth;
 
-				System.out.println("piece sub X: " + xSub);
 				BufferedImage subImg = componentImg.getSubimage(xSub, 0, width,
 						height);
 
 				// draw coords
 				int offset = (imageIndex * pieceWidth);
 				int xDraw = (piece * pieceWidth * imageCount) + offset;
-				System.out.println("piece draw X: " + xDraw);
 				g.drawImage(subImg, xDraw, 0, pieceWidth, mosaicHeight, null);
 			}
 
@@ -197,18 +182,15 @@ public class ImagesToMosaic {
 			if (leftOver > 0) {
 				int xSub = componentImg.getWidth() - components
 						* imagepieceWidth;
-				System.out.println("piece sub x: " + xSub);
 				if (leftOver <= imagepieceWidth)
 					imagepieceWidth = leftOver;
 				BufferedImage subImg = componentImg.getSubimage(xSub, 0,
 						imagepieceWidth, height);
 
 				int xDraw = mosaicWidth - leftOver;
-				System.out.println("piece draw x: " + xDraw);
 				g.drawImage(subImg, xDraw, 0, pieceWidth, mosaicHeight, null);
 
 				leftOver -= imagepieceWidth;
-				System.out.println("leftover = " + leftOver);
 			}
 			imageIndex++;
 		}
@@ -298,7 +280,7 @@ public class ImagesToMosaic {
 	}
 
 	public static void main(String[] args) {
-		// testCompressedSquareMosaic();
+		testCompressedSquareMosaic();
 		testStripeMosaic();
 	}
 
@@ -341,8 +323,8 @@ public class ImagesToMosaic {
 		images.add(im7);
 		images.add(im8);
 		images.add(im9);
-		int mosaicWidth = 1500;
-		int mosaicHeight = 1500;
+		int mosaicWidth = 150;
+		int mosaicHeight = 150;
 		int rows = 3;
 		int squaresPrRow = 3;
 		System.out.println("running the compression mosaic maker.");
@@ -368,12 +350,12 @@ public class ImagesToMosaic {
 		ArrayList<BufferedImage> images = new ArrayList<>();
 		images.add(clean);
 		images.add(dirty);
+		images.add(face);
 		System.out.println("making a 'hard' stripe mosaic of clean and dirty.");
 		int pieces = 10;
-		// ImagesToMosaic.makeHardStripe(images, pieces);
+		ImagesToMosaic.makeHardStripe(images, pieces);
 		System.out.println("hard done.\n\n");
 
-		images.add(face);
 		System.out.println("making a 'blended' stripe mosaic of all three.");
 		pieces = 20;
 		ImagesToMosaic.makeBlendStripe(images, pieces);
