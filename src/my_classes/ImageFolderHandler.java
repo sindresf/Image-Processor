be56c2dd2ef2bce5972cbc737a1494a7792e469d
mr.Fjermestad <sindresfjermestad@gmail.com>
2015-05-21 18:54:51 +1000

    reformated

--- src/my_classes/ImageFolderHandler.java
+++ src/my_classes/ImageFolderHandler.java
@@ -2,70 +2,65 @@ package my_classes;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.io.File;
-
+import java.nio.file.*;
+import java.util.*;
+import java.util.stream.Stream;
 import javax.imageio.ImageIO;
 
 public class ImageFolderHandler {
 
 	public static ArrayList<BufferedImage> getImageFiles(String folderPath) {
-		System.out.println("getting image files from " + folderPath);
 		ArrayList<BufferedImage> images = new ArrayList<>();
 		// All image file extensions acceptable
 		final List<String> extensions = Arrays.asList(".JPG", ".png");
-		try {
-			System.out.println("walking paths");
-			Files.walk(Paths.get(folderPath)).map(Object::toString)
-					.filter(s -> extensions.stream().anyMatch(s::endsWith))
-					.forEach(filePath -> {
-						System.out.println("adding: " + filePath);
-						BufferedImage image = null;
-						try {
-							image = ImageIO.read(new File(filePath));
-							images.add(image);
-							image.flush();
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
 
-					});
+		Stream<String> filePaths = walk(folderPath);
+		Stream<String> filteredPaths = filterStream(filePaths, extensions);
+		filteredPaths.forEach(filePath -> {
+			addImageToList(filePath, images);
+		});
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 		return images;
 	}
 
 	public static ArrayList<BufferedImage> getImageFiles(String folderPath,
 			String extention) {
-		System.out.println("getting image files from " + folderPath);
 		ArrayList<BufferedImage> images = new ArrayList<>();
 		final List<String> extensions = Arrays.asList(extention);
+
+		Stream<String> filePaths = walk(folderPath);
+		Stream<String> filteredPaths = filterStream(filePaths, extensions);
+		filteredPaths.forEach(filePath -> {
+			addImageToList(filePath, images);
+		});
+		return images;
+	}
+
+	private static Stream<String> walk(String folderPath) {
+		Stream<String> stream = null;
 		try {
-			System.out.println("walking paths");
-			Files.walk(Paths.get(folderPath)).map(Object::toString)
-					.filter(s -> extensions.stream().anyMatch(s::endsWith))
-					.forEach(filePath -> {
-						System.out.println("adding: " + filePath);
-						BufferedImage image = null;
-						try {
-							image = ImageIO.read(new File(filePath));
-							images.add(image);
-							image.flush();
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-
-					});
-
+			stream = Files.walk(Paths.get(folderPath)).map(Object::toString);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return images;
+		return stream;
+	}
+
+	private static Stream<String> filterStream(Stream<String> walkStream,
+			List<String> extensions) {
+		return walkStream
+				.filter(s -> extensions.stream().anyMatch(s::endsWith));
+	}
+
+	private static void addImageToList(String filePath,
+			ArrayList<BufferedImage> images) {
+		try {
+			BufferedImage image = ImageIO.read(new File(filePath));
+			images.add(image);
+			image.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
